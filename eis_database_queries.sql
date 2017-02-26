164903352d803741796a84b7fe163f8016625ed2
bharath kollareddy <bkollareddy@gmail.com>
2017-02-25 23:22:47 -0500

    added employee master and users table

--- eis_database_queries.sql
+++ eis_database_queries.sql
@@ -1,6 +1,25 @@
 create user 'testuser' identified by 'test1234';
 GRANT ALL PRIVILEGES ON eis.* TO 'testuser'@'localhost';
 
-CREATE TABLE eis_employee (emp_id varchar(11) NOT NULL,first_name varchar(10) NOT NULL,last_name varchar(10) NOT NULL,created_date date,   created_by varchar(10),PRIMARY KEY (emp_id) );
+CREATE TABLE eis_employee_master (
+  emp_id int(15) NOT NULL,
+  first_name varchar(20) NOT NULL,
+  middle_name varchar(20) NOT NULL,
+  last_name varchar(20) NOT NULL,
+  created_by varchar(20) NOT NULL,
+  created_date varchar(20) NOT NULL,
+  PRIMARY KEY (emp_id)
+);
 
-CREATE TABLE eis_login (user_name varchar(10) NOT NULL, password varchar(10) NOT NULL, created_date date, created_by varchar(10),   PRIMARY KEY (user_name) );
+CREATE TABLE eis_users (
+  user_name varchar(15) NOT NULL,
+  password varchar(15) NOT NULL,
+  role varchar(20) NOT NULL,
+  emp_id int(15) NOT NULL,
+  enabled int(4) NOT NULL DEFAULT '1',
+  created_by varchar(20) NOT NULL,
+  created_date varchar(20) NOT NULL,
+  PRIMARY KEY (user_name),
+  KEY emp_id (emp_id),
+  CONSTRAINT eis_users_ibfk_1 FOREIGN KEY (emp_id) REFERENCES eis_employee_master (emp_id)
+);
